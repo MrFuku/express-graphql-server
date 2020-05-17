8528077205c6ad14976e0f0d735574ec347c3ec6
MrFuku <mmafighter_shun@icloud.com>
2020-05-17 17:59:35 +0900

    mongoDBとの接続

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+const user = 'user'
+const pass = 'password'
+mongoose.connect(`mongodb://${user}:${pass}@mongo/test`)
+mongoose.connection.once('open', () => {
+  console.log('connected mongoDB')
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'))
